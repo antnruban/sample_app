ee7425260d3bf3fcf1b57b52de53bdbb4fdeaaf5
Anton Ruban <a.ruban.documents@gmail.com>
2015-11-02 19:07:09 +0200

    Added unit tests to search_images_helper_spec.rb

--- spec/helpers/search_images_helper_spec.rb
+++ spec/helpers/search_images_helper_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe SearchImagesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "'search_results' method" do
+    it "returns data" do
+      params['search_query'] = "image"
+
+      search_results.should be
+    end
+
+    it "is encoded" do
+      params['search_query'] = "картинка"
+
+      search_results.should be
+    end
+  end
+
+  describe "API URL" do
+    it "is working" do
+      url_answer = open(GOOGLE_IMAGE_API_URL).read
+
+      url_answer.should be
+    end
+  end
 end

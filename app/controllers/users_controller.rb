3340f56604d98edc2a9057205ebc7cb84f16119a
Anton Ruban <a.ruban.documents@gmail.com>
2015-11-10 13:02:46 +0200

    Implemented user fail and success registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-
-  attr_accessor :user
-
   def new
     @user = User.new
   end
@@ -11,11 +8,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      # User saved
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104200249) do
+ActiveRecord::Schema.define(version: 20141211160248) do
 
   create_table "users", force: true do |t|
     t.string   "name"

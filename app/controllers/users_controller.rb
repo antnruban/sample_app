f5a041b9da0c9cde331592d1c70db9ade13cb5be
antnruban <antnruban@gmail.com>
2015-12-13 01:28:25 +0200

    Add autorization.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,9 +21,37 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Your profile updated successfully"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+#######################################################################################################################
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # filters
+
+  def signed_in_user
+    redirect_to signin_path, notice: "Please, sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,6 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-
     describe "with invalid information" do
       before { click_button "Sign In" }
 
@@ -22,11 +21,7 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign In"
-      end
+      before { sign_in(user) }
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
@@ -36,6 +31,81 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign Out"}
         it { should have_link "Sign In" }
+      end
+    end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content "Update your profile" }
+      it { should have_title ("Edit User") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+
+      describe "with valid information" do
+        let(:new_name) { "New Name" }
+        let(:new_email) { "new@post.dom" }
+        before do
+          fill_in "Name",             with: new_name
+          fill_in "Email",            with: new_email
+          fill_in "Password",         with: user.password
+          fill_in "Confirm Password", with: user.password
+          click_button "Save changes"
+        end
+
+        it { should have_title(new_name) }
+        it { should have_selector('div.alert.alert-success') }
+        it { should have_link('Sign Out', href: signout_path) }
+        specify { expect(user.reload.name).to  eq new_name }
+        specify { expect(user.reload.email).to eq new_email }
+      end
+
+      describe "with invalid information" do
+        before { click_button "Save changes" }
+
+        it { should have_content "error" }
+      end
+    end
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign In') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@post.dom") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
       end
     end
   end

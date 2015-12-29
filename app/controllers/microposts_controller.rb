cd061bb89a9b153fabbee6d106c289105ccb758e
antnruban <antnruban@gmail.com>
2015-12-29 13:01:01 +0200

    Add destroy functionality. Finish user microposts. Finish charter 10.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(microposts_params)
@@ -13,6 +14,8 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_path, notice: "Post deleted"
   end
 
   private
@@ -20,5 +23,11 @@ class MicropostsController < ApplicationController
 
   def microposts_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find(params[:id])
+  rescue
+    redirect_to root_path
   end
 end

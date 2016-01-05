8d546f0ee240be17f776b7309ec09a550f9c0c1e
antnruban <antnruban@gmail.com>
2016-01-05 17:13:11 +0200

    Change test max_length value to production, in input checker.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,11 +4,11 @@ window.onload = function() {
   var textarea = $('#micropost_content')[0];
   var div = $('div[class="field"]')[0];
   var submit = $('input[type="submit"][name="commit"]');
-  var maxLength = 5;
+  var maxLength = 140;
   var errorClass = 'field_with_errors';
   var correctClass = 'field';
 
-  textarea.onkeyup = function () {
+  textarea.onkeyup = function() {
     if (textarea.value.length > maxLength) {
       div.className = errorClass;
       submit.prop('disabled', true);

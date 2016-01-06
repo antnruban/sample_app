d9ca9b7b852a8cf38404940e6d58cfac0607697e
antnruban <antnruban@gmail.com>
2016-01-06 03:42:04 +0200

    Add confortable metheds to User model, for follow functionallity.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,23 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: true }
   validates :password, length: { minimum: 6 }
   has_many  :microposts, dependent: :destroy
-  has_many :relationships,foreign_key: "follower_id", dependent: :destroy
+  has_many  :relationships,foreign_key: "follower_id", dependent: :destroy
+  has_many  :followed_users, through: :relationships, source: :followed
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   def User.new_remember_token
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,10 @@ describe User do
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:following?) }
+
 
 
   it { should be_valid }
@@ -160,6 +164,24 @@ describe User do
       relationships.each do |relationship|
         expect(Relationship.where(followed_id: followed.id )).to be_empty
       end
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end

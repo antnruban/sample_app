39e78c6c7c21d1171016c14755392824366088da
antnruban <antnruban@gmail.com>
2016-01-11 17:11:10 +0200

    Configurate "reverse_relationships" for Relationship class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,12 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: true }
   validates :password, length: { minimum: 6 }
   has_many  :microposts, dependent: :destroy
-  has_many  :relationships,foreign_key: "follower_id", dependent: :destroy
+
+  has_many  :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many  :followed_users, through: :relationships, source: :followed
+
+  has_many  :reverse_relationships, foreign_key: "followed_id",  class_name: "Relationship", dependent: :destroy
+  has_many  :followers, through: :reverse_relationships, source: :follower
 
   def feed
     Micropost.where("user_id = ?", id)

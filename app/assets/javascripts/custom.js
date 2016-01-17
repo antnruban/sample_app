bdbb208cf7dcfa8b91320cccb2f7acc189feaf2c
antnruban <antnruban@gmail.com>
2016-01-18 00:21:47 +0200

    Fixed textarea js-validation on home page.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,21 +1,21 @@
 // Input's length checker on Home page.
+(function($) {
+  $(document).ready(function() {
+    var $textarea = $('#micropost_content');
+    var $div = $('div[class="field"]');
+    var $submit = $('input[type="submit"][name="commit"]');
+    var maxLength = 140;
+    var errorClass = 'field_with_errors';
 
-window.onload = function() {
-  var textarea = $('#micropost_content')[0];
-  var div = $('div[class="field"]')[0];
-  var submit = $('input[type="submit"][name="commit"]');
-  var maxLength = 140;
-  var errorClass = 'field_with_errors';
-  var correctClass = 'field';
-
-  textarea.onkeyup = function() {
-    if (textarea.value.length > maxLength) {
-      div.className = errorClass;
-      submit.prop('disabled', true);
-    }
-    else {
-      div.className = correctClass;
-      submit.prop('disabled', false);
-    }
-  }
-}
+    $textarea.on('keyup', function() {
+      if ($(this).val().length > maxLength) {
+        $div.addClass(errorClass);
+        $submit.prop('disabled', true);
+      }
+      else {
+        $div.removeClass(errorClass);
+        $submit.prop('disabled', false);
+      }
+    });
+  });
+})(jQuery);

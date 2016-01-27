2abf504f09e9888751dd9740a706f9e77ccaaa0a
antnruban <antnruban@gmail.com>
2016-01-27 19:07:59 +0200

    Configure smtp settings for Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,15 @@ SampleApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Default mailer host.
+  # Default mailer configurations.
   config.action_mailer.default_url_options = { :host => "http://firstrorapplication.heroku.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => 'smtp.sendgrid.net',
+    :port                 => '587',
+    :authentication       => :plain,
+    :user_name            => ENV["SENDGRID_USERNAME"],
+    :password             => ENV["SENDGRID_PASSWORD"],
+    :enable_starttls_auto => true
+  }
 end
